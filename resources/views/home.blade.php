49d1800273dcebe05db9330755c209585ee420f8
LucasNRama <lucasrama2002@gmail.com>
2024-11-08 16:27:31 -0300

    006

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts/main_layout')
 @section('content')
 
-{{-- instrução Switch --}}
-@switch($value)
-    @case(100)
-        <h1>Valor 100</h1>
-        @break
-    @case(200)
-        <h1>Valor 200</h1>
-        @break
-    @case(300)
-        <h1>Valor 300</h1>
-        @break
-    @default
-        <h1>Outro!</h1>
-@endswitch
+{{-- empty --}}
+@empty($value)
+    <p>NAO EXISTE</p>
+@else
+    <p>EXISTE</p>
+@endempty
+
+{{-- isset --}}
+@isset($values)
+<p>EXISTE A VARIÁVEL</p>
+@else
+<p>NÃO EXISTE A VARIÁVEL</p>
+@endisset
+
+{{-- unless --}}
+@unless($value != 100)
+    <p>OK!!!!!</p>
+@endunless
 
 
 @endsection

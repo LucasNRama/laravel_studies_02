ad147b605c495c5c079bc4c655374016d7503653
LucasNRama <lucasrama2002@gmail.com>
2024-11-08 16:15:12 -0300

    004

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts/main_layout')
 @section('content')
 
-{{-- comentário de blade --}}
-<p><?= $name ?></p>
-<p>{{ $name }}</p>
+{{-- instrução IF simples --}}
+@if($value == 100) 
+<h1>Primeira parte do código</h1>
+@endif
+
+{{-- instrução IF ELSE --}}
+@if($value != 100)
+<h1>Primeira parte do código</h1>
+@else    
+<h1>Segunda parte do código</h1>
+@endif
+
+{{-- instrução IF ELSE --}}
+@if($value < 10)
+    <p>Primeiro</p>
+@elseif($value < 20)
+    <p>Segundo</p>
+@elseif($value > 50)
+    <p>Terceiro</p>
+@elseif($value == 100)
+    <p>Quarto</p>
+@else
+    <p>Quinto</p>
+@endif
+
 @endsection

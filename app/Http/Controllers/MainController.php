c4c2ef71d78c5ddad79dc4731f33c7d384fd6015
LucasNRama <lucasrama2002@gmail.com>
2024-11-08 16:04:21 -0300

    003

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,34 +9,6 @@ class MainController extends Controller
 {
     public function showView() : View
     {
-        // método 1
-       /*  
-       $data = [
-             'name' => "joão Ribeiro",
-             'phone' => "123456789" 
-        ];
-        return view('admin.newPage3', $data); 
-        */
-        
-        // método 2
-        /* 
-        return view('admin.newPage3', [
-            'name' => "joão Ribeiro",
-            'phone' => "123456789" 
-        ]); 
-        */
-
-        // método 3
-        /* 
-        return view('admin.newPage3')
-                ->with('name', "joão ribeiro")
-                ->with('phone', "1234567890"); 
-        */
-
-        // método 4
-        $name = "João Ribeiro";
-        $phone = "1234567890";
-
-        return view('admin.newPage3', compact('name', 'phone'));
+        return view('home', ['name' => 'John Doe']);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts/main_layout')
 @section('content')
 
-<p class="display-1 text-center">Hello World!</p>
-
+{{-- comentário de blade --}}
+<p><?= $name ?></p>
+<p>{{ $name }}</p>
 @endsection

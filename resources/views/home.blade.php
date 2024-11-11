c448ea8c94af96e4e0fc5d495555c66881081056
LucasNRama <lucasrama2002@gmail.com>
2024-11-11 14:41:28 -0300

    010

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,37 @@
 @extends('layouts/main_layout')
 @section('content')
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-6">
+@production
+    <p>Estou em ambiente de produção</p>
+@else
+    <p>Não estou em produção</p>
+@endproduction
 
-            <form action="{{ route('submit')}}" method="POST">
+@env(['local', 'development'])
+    <p>Estou no ambiente {{ env('APP_ENV') }}</p>
+@endenv
 
-                @csrf 
+{{-- formulário  --}}
+<form action="{{ route('submit') }}" method="post">
+    @csrf
 
-                <div class="mb-3">
-                    <label class="form-label">Username:</label>
-                    <input type="text" name="username" class="form-control">
-                </div>
-
-                <div class="mb-3">
-                    <label class="form-label">Password:</label>
-                    <input type="password" name="password" class="form-control">
-                </div>
-
-                <div class="mb-3">
-                    <button class="btn btn-primary">Login</button>
-                </div>
-
-            </form>
-
-        </div>
+    <div>
+        <input type="text" name="name">
+        @error('name')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
     </div>
-</div>
 
-{{-- executar PHP dentro de uma view --}}
-@php
-    $valor = 100;
-    $valor1 = '<span class="text-warning">' . $valor . '</span>';
-    $nome = "joao"
-@endphp
+    <div>
+        <input type="text" name="country">
+        @error('country')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
 
-<h3>{{ $valor }}</h3>
-<h3>{!! $valor1 !!}</h3>
-<h3>{{ $valor * 1000}}</h3>
+    <button type="submit">Enviar</button>
 
-<h3> $nome tem <span class="text-info"> {{ strlen($nome) }}</span> caracteres </h3>
+    
+</form>
 
 @endsection
